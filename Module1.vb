ff0891cc217b9c3ce276895754c1d906938b3c0c
tomhornyak <thomas.hornyak@gmail.com>
2025-05-07 18:28:59 -0400

    Moved Repository

---
+++ Module1.vb
@@ -0,0 +1,324 @@
+﻿
+Imports System.Runtime.InteropServices
+
+Module Module1
+    Public Enum HRESULT As Integer
+        S_OK = 0
+        S_FALSE = 1
+        E_NOINTERFACE = &H80004002
+        E_NOTIMPL = &H80004001
+        E_FAIL = &H80004005
+        E_UNEXPECTED = &H8000FFFF
+        E_OUTOFMEMORY = &H8007000E
+    End Enum
+
+    <StructLayout(LayoutKind.Sequential)>
+    Public Structure POINT
+        Public X As Integer
+        Public Y As Integer
+    End Structure
+
+    <StructLayout(LayoutKind.Sequential)>
+    Public Structure RECT
+        Public left As Integer
+        Public top As Integer
+        Public right As Integer
+        Public bottom As Integer
+        Public Sub New(Left As Integer, Top As Integer, Right As Integer, Bottom As Integer)
+            Left = Left
+            Top = Top
+            Right = Right
+            Bottom = Bottom
+        End Sub
+    End Structure
+
+    Public Const SWC_DESKTOP = &H8
+
+    Public Enum ShellWindowFindWindowOptions As Integer
+        SWFO_NEEDDISPATCH = &H1
+        SWFO_INCLUDEPENDING = &H2
+        SWFO_COOKIEPASSED = &H4
+    End Enum
+
+    <DllImport("Shlwapi.dll", SetLastError:=True, CharSet:=CharSet.Auto)>
+    Public Function IUnknown_QueryService(ByVal punk As IntPtr, ByRef guidService As Guid, ByRef riid As Guid, <Out> ByRef ppvOut As IntPtr) As HRESULT
+    End Function
+
+    <ComImport>
+    <Guid("00000114-0000-0000-C000-000000000046")>
+    <InterfaceType(ComInterfaceType.InterfaceIsIUnknown)>
+    Interface IOleWindow
+        Function GetWindow(<Out> ByRef phwnd As IntPtr) As HRESULT
+        Function ContextSensitiveHelp(fEnterMode As Boolean) As HRESULT
+    End Interface
+
+    <ComImport>
+    <Guid("000214E2-0000-0000-C000-000000000046")>
+    <InterfaceType(ComInterfaceType.InterfaceIsIUnknown)>
+    Interface IShellBrowser
+        Inherits IOleWindow
+
+        Overloads Function GetWindow(<Out> ByRef phwnd As IntPtr) As HRESULT
+        Overloads Function ContextSensitiveHelp(fEnterMode As Boolean) As HRESULT
+        Function InsertMenusSB(IntPtrShared As IntPtr, ByRef lpMenuWidths As OLEMENUGROUPWIDTHS) As HRESULT
+        Function SetMenuSB(IntPtrShared As IntPtr, holemenuRes As IntPtr, IntPtrActiveObject As IntPtr) As HRESULT
+        Function RemoveMenusSB(IntPtrShared As IntPtr) As HRESULT
+        Function SetStatusTextSB(pszStatusText As String) As HRESULT
+        Function EnableModelessSB(fEnable As Boolean) As HRESULT
+        Function TranslateAcceleratorSB(pmsg As MSG, wID As UInt16) As HRESULT
+        Function BrowseObject(pidl As IntPtr, wFlags As UInteger) As HRESULT
+        Function GetViewStateStream(grfMode As UInteger, <Out> ByRef ppStrm As ComTypes.IStream) As HRESULT
+        Function GetControlWindow(id As UInteger, <Out> ByRef pIntPtr As IntPtr) As HRESULT
+        Function SendControlMsg(id As UInteger, uMsg As UInteger, wParam As Integer, lParam As IntPtr, <Out> ByRef pret As IntPtr) As HRESULT
+        Function QueryActiveShellView(<Out> ByRef ppshv As IShellView) As HRESULT
+        Function OnViewWindowActive(pshv As IShellView) As HRESULT
+        Function SetToolbarItems(lpButtons As TBBUTTON, nButtons As UInteger, uFlags As UInteger) As HRESULT
+    End Interface
+
+    <ComImport>
+    <Guid("000214E3-0000-0000-C000-000000000046")>
+    <InterfaceType(ComInterfaceType.InterfaceIsIUnknown)>
+    Interface IShellView
+        Inherits IOleWindow
+
+        Overloads Function GetWindow(<Out> ByRef phwnd As IntPtr) As HRESULT
+        Overloads Function ContextSensitiveHelp(fEnterMode As Boolean) As HRESULT
+        Function TranslateAccelerator(pmsg As MSG) As HRESULT
+        Function EnableModeless(fEnable As Boolean) As HRESULT
+        Function UIActivate(uState As UInteger) As HRESULT
+        Function Refresh() As HRESULT
+        Function CreateViewWindow(psvPrevious As IShellView, pfs As FOLDERSETTINGS, psb As IShellBrowser, prcView As RECT, <Out> ByRef pIntPtr As IntPtr) As HRESULT
+        Function DestroyViewWindow() As HRESULT
+        Function GetCurrentInfo(<Out> ByRef pfs As FOLDERSETTINGS) As HRESULT
+        Function AddPropertySheetPages(dwReserved As Integer, pfn As IntPtr, lparam As IntPtr) As HRESULT
+        Function SaveViewState() As HRESULT
+        Function SelectItem(pidlItem As IntPtr, uFlags As SVSIF) As HRESULT
+        Function GetItemObject(uItem As UInteger, ByRef riid As Guid, <Out> <MarshalAs(UnmanagedType.[Interface])> ByRef ppv As Object) As HRESULT
+    End Interface
+
+    <StructLayout(LayoutKind.Sequential)>
+    Public Structure MSG
+        Public hwnd As IntPtr
+        Public message As UInteger
+        Public wParam As Integer
+        Public lParam As IntPtr
+        Public time As Integer
+        Public pt As POINT
+    End Structure
+
+    <StructLayout(LayoutKind.Sequential)>
+    Public Structure FOLDERSETTINGS
+        Public ViewMode As UInteger
+        Public fFlags As UInteger
+    End Structure
+
+    Public Enum SVSIF
+        SVSI_DESELECT = 0
+        SVSI_SELECT = &H1
+        SVSI_EDIT = &H3
+        SVSI_DESELECTOTHERS = &H4
+        SVSI_ENSUREVISIBLE = &H8
+        SVSI_FOCUSED = &H10
+        SVSI_TRANSLATEPT = &H20
+        SVSI_SELECTIONMARK = &H40
+        SVSI_POSITIONITEM = &H80
+        SVSI_CHECK = &H100
+        SVSI_CHECK2 = &H200
+        SVSI_KEYBOARDSELECT = &H401
+        SVSI_NOTAKEFOCUS = &H40000000
+    End Enum
+
+    <StructLayout(LayoutKind.Sequential)>
+    Public Structure TBBUTTON
+        Public iBitmap As Integer
+        Public idCommand As Integer
+        Public fsState As Byte
+        Public fsStyle As Byte
+        Public bReserved0 As Byte
+        Public bReserved1 As Byte
+        Public dwData As Integer
+        Public iString As IntPtr
+    End Structure
+
+    <StructLayout(LayoutKind.Sequential)>
+    Public Structure OLEMENUGROUPWIDTHS
+        <MarshalAs(UnmanagedType.U2, SizeConst:=6)>
+        Public width As Integer()
+    End Structure
+
+    <ComImport>
+    <Guid("cde725b0-ccc9-4519-917e-325d72fab4ce")>
+    <InterfaceType(ComInterfaceType.InterfaceIsIUnknown)>
+    Public Interface IFolderView
+        Function GetCurrentViewMode(ByRef pViewMode As UInteger) As HRESULT
+        Function SetCurrentViewMode(ViewMode As UInteger) As HRESULT
+        Function GetFolder(ByRef riid As Guid, ByRef ppv As IntPtr) As HRESULT
+        Function Item(iItemIndex As Integer, ByRef ppidl As IntPtr) As HRESULT
+        Function ItemCount(uFlags As UInteger, ByRef pcItems As Integer) As HRESULT
+        Function Items(uFlags As UInteger, ByRef riid As Guid, ByRef ppv As IntPtr) As HRESULT
+        Function GetSelectionMarkedItem(ByRef piItem As Integer) As HRESULT
+        Function GetFocusedItem(ByRef piItem As Integer) As HRESULT
+        Function GetItemPosition(pidl As IntPtr, ByRef ppt As POINT) As HRESULT
+        Function GetSpacing(ByRef ppt As POINT) As HRESULT
+        Function GetDefaultSpacing(ByRef ppt As POINT) As HRESULT
+        Function GetAutoArrange() As HRESULT
+        Function SelectItem(iItem As Integer, dwFlags As Integer) As HRESULT
+        Function SelectAndPositionItems(cidl As UInteger, apidl As IntPtr, apt As POINT, dwFlags As Integer) As HRESULT
+    End Interface
+
+    Public Enum SVGIO
+        SVGIO_BACKGROUND = 0
+        SVGIO_SELECTION = &H1
+        SVGIO_ALLVIEW = &H2
+        SVGIO_CHECKED = &H3
+        SVGIO_TYPE_MASK = &HF
+        SVGIO_FLAG_VIEWORDER = &H80000000
+    End Enum
+
+    <ComImport, InterfaceType(ComInterfaceType.InterfaceIsIUnknown), Guid("000214E6-0000-0000-C000-000000000046")>
+    Interface IShellFolder
+        Function ParseDisplayName(hwnd As IntPtr, pbc As IntPtr, <MarshalAs(UnmanagedType.LPWStr)> pszDisplayName As String, <[In], Out> ByRef pchEaten As UInteger, <Out> ByRef ppidl As IntPtr, <[In], Out> ByRef pdwAttributes As SFGAO) As HRESULT
+        Function EnumObjects(hwnd As IntPtr, grfFlags As SHCONTF, <Out> ByRef ppenumIDList As IEnumIDList) As HRESULT
+        Function BindToObject(pidl As IntPtr, pbc As IntPtr, <[In]> ByRef riid As Guid, <Out> <MarshalAs(UnmanagedType.[Interface])> ByRef ppv As Object) As HRESULT
+        Function BindToStorage(pidl As IntPtr, pbc As IntPtr, <[In]> ByRef riid As Guid, <Out> <MarshalAs(UnmanagedType.[Interface])> ByRef ppv As Object) As HRESULT
+        Function CompareIDs(lParam As IntPtr, pidl1 As IntPtr, pidl2 As IntPtr) As HRESULT
+        Function CreateViewObject(hwndOwner As IntPtr, <[In]> ByRef riid As Guid, <Out> <MarshalAs(UnmanagedType.[Interface])> ByRef ppv As Object) As HRESULT
+        Function GetAttributesOf(cidl As UInteger, apidl As IntPtr, <[In], Out> ByRef rgfInOut As SFGAO) As HRESULT
+        Function GetUIObjectOf(hwndOwner As IntPtr, cidl As UInteger, ByRef apidl As IntPtr, <[In]> ByRef riid As Guid, <[In], Out> ByRef rgfReserved As UInteger, <Out> ByRef ppv As IntPtr) As HRESULT
+        Function GetDisplayNameOf(pidl As IntPtr, uFlags As SHGDNF, <Out> ByRef pName As STRRET) As HRESULT
+        Function SetNameOf(hwnd As IntPtr, pidl As IntPtr, <MarshalAs(UnmanagedType.LPWStr)> pszName As String, uFlags As SHGDNF, <Out> ByRef ppidlOut As IntPtr) As HRESULT
+    End Interface
+
+    Public Enum SHCONTF
+        SHCONTF_CHECKING_FOR_CHILDREN = &H10
+        SHCONTF_FOLDERS = &H20
+        SHCONTF_NONFOLDERS = &H40
+        SHCONTF_INCLUDEHIDDEN = &H80
+        SHCONTF_INIT_ON_FIRST_NEXT = &H100
+        SHCONTF_NETPRINTERSRCH = &H200
+        SHCONTF_SHAREABLE = &H400
+        SHCONTF_STORAGE = &H800
+        SHCONTF_NAVIGATION_ENUM = &H1000
+        SHCONTF_FASTITEMS = &H2000
+        SHCONTF_FLATLIST = &H4000
+        SHCONTF_ENABLE_ASYNC = &H8000
+    End Enum
+
+    Public Enum SFGAO
+        CANCOPY = &H1
+        CANMOVE = &H2
+        CANLINK = &H4
+        STORAGE = &H8
+        CANRENAME = &H10
+        CANDELETE = &H20
+        HASPROPSHEET = &H40
+        DROPTARGET = &H100
+        CAPABILITYMASK = &H177
+        ENCRYPTED = &H2000
+        ISSLOW = &H4000
+        GHOSTED = &H8000
+        LINK = &H10000
+        SHARE = &H20000
+        [READONLY] = &H40000
+        HIDDEN = &H80000
+        DISPLAYATTRMASK = &HFC000
+        STREAM = &H400000
+        STORAGEANCESTOR = &H800000
+        VALIDATE = &H1000000
+        REMOVABLE = &H2000000
+        COMPRESSED = &H4000000
+        BROWSABLE = &H8000000
+        FILESYSANCESTOR = &H10000000
+        FOLDER = &H20000000
+        FILESYSTEM = &H40000000
+        HASSUBFOLDER = &H80000000
+        CONTENTSMASK = &H80000000
+        STORAGECAPMASK = &H70C50008
+        PKEYSFGAOMASK = &H81044000
+    End Enum
+
+    Public Enum SHGDNF
+        SHGDN_NORMAL = 0
+        SHGDN_INFOLDER = &H1
+        SHGDN_FOREDITING = &H1000
+        SHGDN_FORADDRESSBAR = &H4000
+        SHGDN_FORPARSING = &H8000
+    End Enum
+
+    <StructLayout(LayoutKind.Explicit, Size:=264)>
+    Public Structure STRRET
+        <FieldOffset(0)>
+        Public uType As UInteger
+        <FieldOffset(4)>
+        Public pOleStr As IntPtr
+        <FieldOffset(4)>
+        Public uOffset As UInteger
+        <FieldOffset(4)>
+        Public cString As IntPtr
+    End Structure
+
+    <ComImport, InterfaceType(ComInterfaceType.InterfaceIsIUnknown), Guid("000214F2-0000-0000-C000-000000000046")>
+    Interface IEnumIDList
+        <PreserveSig()>
+        Function [Next](celt As UInteger, <Out> ByRef rgelt As IntPtr, <Out> ByRef pceltFetched As Integer) As HRESULT
+        <PreserveSig()>
+        Function Skip(celt As UInteger) As HRESULT
+        Sub Reset()
+        Function Clone() As IEnumIDList
+    End Interface
+
+    <DllImport("Shlwapi.dll", CharSet:=CharSet.Unicode, SetLastError:=True)>
+    Public Function StrRetToBuf(ByRef pstr As STRRET, ByVal pidl As IntPtr, ByVal pszBuf As System.Text.StringBuilder, <MarshalAs(UnmanagedType.U4)> ByVal cchBuf As UInteger) As HRESULT
+    End Function
+    Sub Main()
+        Dim pShellWindows = New SHDocVw.ShellWindows()
+        Dim hWndDesktop As IntPtr
+        Dim oDispatch = pShellWindows.FindWindowSW(0, Nothing, SWC_DESKTOP, hWndDesktop, ShellWindowFindWindowOptions.SWFO_NEEDDISPATCH)
+        Dim pDispatch = Marshal.GetIDispatchForObject(oDispatch)
+        Dim pvOut As IntPtr = IntPtr.Zero
+        Dim SID_STopLevelBrowser As Guid = New Guid("4C96BE40-915C-11CF-99D3-00AA004AE837")
+        Dim IID_IShellBrowser As Guid = New Guid("000214E2-0000-0000-C000-000000000046")
+        IUnknown_QueryService(pDispatch, SID_STopLevelBrowser, IID_IShellBrowser, pvOut)
+        If (pvOut <> IntPtr.Zero) Then
+            Dim psb As IShellBrowser = TryCast(Marshal.GetObjectForIUnknown(pvOut), IShellBrowser)
+            If (psb IsNot Nothing) Then
+                Dim psv As IShellView = Nothing
+                Dim hr As HRESULT = psb.QueryActiveShellView(psv)
+                If (hr = HRESULT.S_OK) Then
+                    Dim pFolderView = TryCast(psv, IFolderView)
+                    Dim pShellFolderPtr As IntPtr = IntPtr.Zero
+                    hr = pFolderView.GetFolder(GetType(IShellFolder).GUID, pShellFolderPtr)
+                    Dim psf As IShellFolder = TryCast(Marshal.GetObjectForIUnknown(pShellFolderPtr), IShellFolder)
+                    Dim nItemCount As Integer = 0
+                    hr = pFolderView.ItemCount(SVGIO.SVGIO_ALLVIEW, nItemCount)
+                    If (hr = HRESULT.S_OK) Then
+                        For i As Integer = 0 To nItemCount - 1
+                            Dim pidlItem As IntPtr
+                            hr = pFolderView.Item(i, pidlItem)
+                            If (hr = HRESULT.S_OK) Then
+                                Console.WriteLine(String.Format("Item {0}", (i + 1).ToString()))
+                                Dim strretFolderName As STRRET
+                                hr = psf.GetDisplayNameOf(pidlItem, SHGDNF.SHGDN_NORMAL, strretFolderName)
+                                Dim sDisplayName As String = Nothing
+                                If (hr = HRESULT.S_OK) Then
+                                    Dim sbDisplayName As System.Text.StringBuilder
+                                    sbDisplayName = New System.Text.StringBuilder(256)
+                                    StrRetToBuf(strretFolderName, pidlItem, sbDisplayName, sbDisplayName.Capacity)
+                                    sDisplayName = sbDisplayName.ToString
+                                End If
+                                Console.WriteLine(String.Format("{0}Name : {1}", vbTab, sDisplayName))
+                            End If
+                            Dim nPos As POINT = Nothing
+                            hr = pFolderView.GetItemPosition(pidlItem, nPos)
+                            Console.WriteLine(String.Format("{0}Position : ({1},{2})", vbTab, nPos.X, nPos.Y))
+                        Next
+                    End If
+                    Marshal.ReleaseComObject(psf)
+                    Marshal.ReleaseComObject(psv)
+                End If
+                Marshal.ReleaseComObject(psb)
+            End If
+        End If
+    End Sub
+
+End Module
---
+++ SortIcons.cs
@@ -0,0 +1,270 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+namespace SortDesktopIcons
+{
+
+    static class SortIcons
+    {
+        [ComImport, Guid("6D5140C1-7436-11CE-8034-00AA006009FA"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+        public interface IServiceProvider
+        {
+            [return: MarshalAs(UnmanagedType.IUnknown)]
+            object QueryService([MarshalAs(UnmanagedType.LPStruct)] Guid service, [MarshalAs(UnmanagedType.LPStruct)] Guid riid);
+        }
+
+        // note: for the following interfaces, not all methods are defined as we don't use them here
+        [ComImport, Guid("000214E2-0000-0000-C000-000000000046"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+        public interface IShellBrowser
+        {
+            void _VtblGap1_12(); // skip 12 methods https://stackoverflow.com/a/47567206/403671
+
+            [return: MarshalAs(UnmanagedType.IUnknown)]
+            object QueryActiveShellView();
+        }
+
+        [ComImport, Guid("cde725b0-ccc9-4519-917e-325d72fab4ce"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+        public interface IFolderView
+        {
+            void _VtblGap1_3(); // skip 3 methods
+
+            IntPtr Item(int iItemIndex);
+            int ItemCount(uint uFlags = 0);
+
+            void _VtblGap2_3(); // skip 2 methods
+
+            void GetItemPosition(IntPtr pidl, out POINT ppt);
+
+            void _VtblGap1_4(); // skip 4 methods
+
+            void SelectAndPositionItems(int cidl, [MarshalAs(UnmanagedType.LPArray, SizeParamIndex = 0)] IntPtr[] apidl, [MarshalAs(UnmanagedType.LPArray, SizeParamIndex = 0)] POINT[] apt, SVSIF dwFlags);
+
+            // more undefined methods
+        }
+
+        public enum FOLDERFLAGS
+        {
+            FWF_AUTOARRANGE = 0x1,
+            FWF_SNAPTOGRID = 0x4
+        }
+        [ComImport, Guid("1af3a467-214f-4298-908e-06b03e0b39f9"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+        public interface IFolderView2
+        {
+            //void _VtblGap1_26(); // skip 14 (IFolderView) + 12 methods
+            void _VtblGap1_21(); // skip 14 (IFolderView) + 12 methods
+            uint SetCurrentFolderFlags(int dwMask, int dwFlags);
+            void _VtblGap1_4();
+            IShellItem GetItem(int iItemIndex, [MarshalAs(UnmanagedType.LPStruct)] Guid riid);
+
+            // more undefined methods
+        }
+
+        [ComImport, Guid("43826d1e-e718-42ee-bc55-a1e261c37bfe"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+        public interface IShellItem
+        {
+            [return: MarshalAs(UnmanagedType.IUnknown)]
+            object BindToHandler(System.Runtime.InteropServices.ComTypes.IBindCtx pbc, [MarshalAs(UnmanagedType.LPStruct)] Guid bhid, [MarshalAs(UnmanagedType.LPStruct)] Guid riid);
+
+            IShellItem GetParent();
+
+            [return: MarshalAs(UnmanagedType.LPWStr)]
+            string GetDisplayName(SIGDN sigdnName);
+
+            [return: MarshalAs(UnmanagedType.U4)]
+            uint GetAttributesOf(uint sfgaoMask);
+
+            // more undefined methods
+        }
+
+        public struct POINT
+        {
+            public int x;
+            public int y;
+
+            //Eliminate stupit warning about uninitialized y
+            public POINT()
+            {
+                x = -1;
+                y = -1;
+            }
+            public override string ToString()
+            {
+                return $"({x},{y})";
+            }
+        }
+
+        public enum SIGDN
+        {
+            SIGDN_NORMALDISPLAY,
+            SIGDN_PARENTRELATIVEPARSING,
+            SIGDN_DESKTOPABSOLUTEPARSING,
+            SIGDN_PARENTRELATIVEEDITING,
+            SIGDN_DESKTOPABSOLUTEEDITING,
+            SIGDN_FILESYSPATH,
+            SIGDN_URL,
+            SIGDN_PARENTRELATIVEFORADDRESSBAR,
+            SIGDN_PARENTRELATIVE,
+            SIGDN_PARENTRELATIVEFORUI
+        }
+
+        [Flags]
+        public enum SVSIF
+        {
+            SVSI_DESELECT = 0,
+            SVSI_SELECT = 0x1,
+            SVSI_EDIT = 0x3,
+            SVSI_DESELECTOTHERS = 0x4,
+            SVSI_ENSUREVISIBLE = 0x8,
+            SVSI_FOCUSED = 0x10,
+            SVSI_TRANSLATEPT = 0x20,
+            SVSI_SELECTIONMARK = 0x40,
+            SVSI_POSITIONITEM = 0x80,
+            SVSI_CHECK = 0x100,
+            SVSI_CHECK2 = 0x200,
+            SVSI_KEYBOARDSELECT = 0x401,
+            SVSI_NOTAKEFOCUS = 0x40000000
+        }
+        public enum SFGAO
+        {
+            CANCOPY = 0x1,
+            CANMOVE = 0x2,
+            CANLINK = 0x4,
+            STORAGE = 0x8,
+            CANRENAME = 0x10,
+            CANDELETE = 0x20,
+            HASPROPSHEET = 0x40,
+            DROPTARGET = 0x100,
+            CAPABILITYMASK = 0x177,
+            ENCRYPTED = 0x2000,
+            ISSLOW = 0x4000,
+            GHOSTED = 0x8000,
+            LINK = 0x10000,
+            SHARE = 0x20000,
+            READONLY = 0x40000,
+            HIDDEN = 0x80000,
+            DISPLAYATTRMASK = 0xFC000,
+            STREAM = 0x400000,
+            STORAGEANCESTOR = 0x800000,
+            VALIDATE = 0x1000000,
+            REMOVABLE = 0x2000000,
+            COMPRESSED = 0x4000000,
+            BROWSABLE = 0x8000000,
+            FILESYSANCESTOR = 0x10000000,
+            FOLDER = 0x20000000,
+            FILESYSTEM = 0x40000000,
+            HASSUBFOLDER = unchecked((int)0x80000000),
+            CONTENTSMASK = unchecked((int)0x80000000),
+            STORAGECAPMASK = 0x70C50008,
+            PKEYSFGAOMASK = unchecked((int)0x81044000)
+        }
+        struct icon
+        {
+            public int index;
+            public string name;
+            public POINT locn;
+            public IntPtr pid;
+            public string type;
+
+            public override string ToString()
+            {
+                return $"Index: {index}, {type}, Name: {name}, Location: {locn}";
+            }
+        }
+
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern IntPtr SendMessage(IntPtr hWnd, uint Msg, IntPtr wParam, IntPtr lParam);
+        [DllImport("Kernel32.dll", CallingConvention = CallingConvention.StdCall, SetLastError = true)]
+        private static extern IntPtr GetConsoleWindow();
+
+        [DllImport("User32.dll", CallingConvention = CallingConvention.StdCall, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool ShowWindow([In] IntPtr hWnd, [In] Int32 nCmdShow);
+
+        const string SHELL_CLASS_NAME = "Progman";
+        const uint WM_COMMAND = 0x111;
+        const int CMD_SORT_BY_NAME = 0x800020;
+
+        [STAThread]
+        public static void Main()
+        {
+            IntPtr handle = GetConsoleWindow();
+            ShowWindow(handle, 6);
+
+            IntPtr desktopHandle = FindWindow(SHELL_CLASS_NAME, null);
+
+            dynamic app = Activator.CreateInstance(Type.GetTypeFromProgID("Shell.Application"));
+            var windows = app.Windows;
+
+            const int SWC_DESKTOP = 8;
+            const int SWFO_NEEDDISPATCH = 1;
+            var hwnd = 0;
+            var disp = windows.FindWindowSW(Type.Missing, Type.Missing, SWC_DESKTOP, ref hwnd, SWFO_NEEDDISPATCH);
+
+            var sp = (IServiceProvider)disp;
+            var SID_STopLevelBrowser = new Guid("4c96be40-915c-11cf-99d3-00aa004ae837");
+
+            var browser = (IShellBrowser)sp.QueryService(SID_STopLevelBrowser, typeof(IShellBrowser).GUID);
+            var view = (IFolderView)browser.QueryActiveShellView();
+            var view2 = (IFolderView2)view;
+
+            view2.SetCurrentFolderFlags((int)(FOLDERFLAGS.FWF_SNAPTOGRID | FOLDERFLAGS.FWF_AUTOARRANGE),
+                (int)(FOLDERFLAGS.FWF_SNAPTOGRID | FOLDERFLAGS.FWF_AUTOARRANGE));
+            if (desktopHandle != IntPtr.Zero)
+            {
+                SendMessage(desktopHandle, WM_COMMAND, new IntPtr(CMD_SORT_BY_NAME), IntPtr.Zero);
+                Console.WriteLine("Desktop icons sorted by name.");
+            }
+            view2.SetCurrentFolderFlags((int)(FOLDERFLAGS.FWF_AUTOARRANGE), 0);
+            var icons = new List<icon>();
+
+            // get all items, dump & sets their position (here y+= 150)
+            for (var i = 0; i < view.ItemCount(); i++)
+            {
+                // get some item's info to be able to determine if we want to move it or not
+                var item = view2.GetItem(i, typeof(IShellItem).GUID);
+                uint attrib = (uint)SFGAO.FOLDER;
+                var type = item.GetAttributesOf(attrib) == 0 ? "File" : "Folder";
+                var displayName = item.GetDisplayName(SIGDN.SIGDN_DESKTOPABSOLUTEPARSING);
+
+                var pidl = view.Item(i);
+                view.GetItemPosition(pidl, out var pt);
+
+                var ic = new icon { index = i, locn = pt, name = displayName, pid = pidl, type = type };
+                icons.Add(ic);
+                Console.WriteLine(ic.ToString());
+            }
+            var byPos = new List<icon>();
+            byPos =
+                icons.OrderBy(icon => icon.locn.x)
+                .ThenBy(icon => icon.locn.y)
+                .ToList();
+
+            var sortedIcons = new List<icon>();
+            sortedIcons =
+                byPos.OrderByDescending(icon => icon.type)
+                .ThenBy(icon => icon.name)
+                .ToList();
+            var ptrs = new List<IntPtr>();
+            var locns = new List<POINT>();
+            for (int i = 0; i < sortedIcons.Count(); i++)
+            {
+                var tempIcon = sortedIcons[i]; // Create a temporary variable
+                tempIcon.locn = byPos[i].locn; // Modify the temporary variable
+                sortedIcons[i] = tempIcon;
+                ptrs.Add(tempIcon.pid);
+                locns.Add(tempIcon.locn);
+            }
+            IntPtr[] apidl = ptrs.ToArray();
+            POINT[] apt = locns.ToArray();
+
+            view.SelectAndPositionItems(apidl.Length, apidl, apt, SVSIF.SVSI_POSITIONITEM);
+
+        }
+
+    }
+}
+
